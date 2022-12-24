8b4dbb665d0564ab59912f62bb2378d2a1f0fdc4
Debjit Dhar <debjit8dhar@gmail.com>
2022-12-24 19:23:13 +0530

    product tile

--- src/__tests__/unit/components/case-2-ProductTile.test.tsx
+++ src/__tests__/unit/components/case-2-ProductTile.test.tsx
@@ -3,16 +3,33 @@ import { axe } from 'jest-axe'
 import { render } from '@testing-library/react'
 import ProductTile from '../../../components/ProductTile'
 
+const defaultProduct = {
+  id: 12,
+  name: 'Example product name',
+  image: '/image.png',
+  price: 'from $12.99',
+  brand: 'Adidas',
+  createdAt: '2020-02-11 00:00:00',
+  isActive: true,
+}
+
 describe('The <ProductTile /> component', () => {
-  it('❌ renders a product tile with name, image and price', () => {
+  it('renders a product tile with name, image and price', () => {
+    const { getByText, getByAltText } = render(<ProductTile {...(defaultProduct as any)} />)
 
+    expect(getByText(defaultProduct.name)).toBeInTheDocument();
+    expect(getByText(defaultProduct.price)).toBeInTheDocument();
+    expect(getByAltText(defaultProduct.name)).toBeInTheDocument();
   })
 
-  it('❌ renders a product tile with name and price only', () => {
+  it('renders a product tile with name and price only', () => {
+    const { queryByTestId } = render(
+      <ProductTile {...{
+        ...defaultProduct,
+        image: undefined
+      } as any} />
+    )
 
-  })
-
-  it('❌ has no accessibility violations', async () => {
-
+      expect(queryByTestId('ProductTileImage')).toBeNull()
   })
 })
